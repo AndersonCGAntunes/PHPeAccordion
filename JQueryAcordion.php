dd8ff48699e9d09362ab7bb9c831a6f3853a6d82
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2022-04-19 20:39:19 -0300

    Arquivos comitados para o GitHub

---
+++ JQueryAcordion.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		<title>JQuery Accordion</title>
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script>
+		<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
+		<script type="text/javascript">
+			$(function(){
+				$("#accordion").accordion();
+			});
+		</script>
+	</head>
+	<body>
+		<?php
+		echo "<div id=\"accordion\">
+			<h3><a href=\"#\">Seção 1</a></h3>
+			<div>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut vehicula nisl. Duis gravida sem eget arcu convallis, a congue orci efficitur. Sed imperdiet, neque quis tristique vestibulum, elit elit vehicula nibh, nec lacinia mauris eros eu odio. Mauris ornare laoreet auctor. Nullam molestie auctor massa, luctus mollis erat congue nec. Proin tincidunt purus quis pellentesque fringilla. Suspendisse potenti. Nunc elit quam, consequat sed arcu non, ullamcorper luctus sem. Aliquam id lectus consectetur, ultrices ligula vulputate, rutrum erat. Curabitur eu aliquam enim. Nulla lobortis quis urna nec egestas. Cras enim leo, auctor ac arcu condimentum, dictum tempus tellus. Morbi rhoncus quis lacus at eleifend. Vestibulum viverra enim ex, eu ultricies velit gravida vitae. Duis nisi velit, mollis at orci et, egestas consequat sem. Nullam purus lorem, pretium sed vulputate vel, sodales quis elit.
+			</div>
+			<h3><a href=\"#\">Seção 2</a></h3>
+			<div>
+				Morbi venenatis justo id ipsum placerat, at scelerisque nibh elementum. Morbi viverra bibendum fermentum. Pellentesque tortor purus, facilisis in quam non, pharetra pharetra erat. Integer orci sem, tincidunt sit amet metus a, placerat vehicula ante. Duis fringilla, magna in euismod tincidunt, velit purus laoreet odio, vel eleifend libero orci vel erat. Cras fermentum at lorem laoreet blandit. Nunc eu dignissim est. Sed suscipit magna eu consectetur rutrum. Etiam auctor lectus vel sapien commodo auctor.
+			</div>
+			<h3><a href=\"#\">Seção 3</a></h3>
+			<div>
+				Vestibulum ligula lorem, aliquet commodo nisi a, sagittis dignissim arcu. Nunc at imperdiet sem. Quisque et dignissim neque, a accumsan sem. Nulla vel ultrices nisl, in placerat nunc. Vivamus hendrerit elit eu justo gravida, ut varius magna iaculis. Mauris eu purus lorem. Fusce dapibus mollis ornare. Nunc dolor arcu, tempor ut nulla sit amet, vulputate viverra libero. Vivamus elementum nulla ac turpis interdum mattis. Vivamus suscipit arcu eget risus posuere tincidunt. Maecenas at purus ac nisi varius volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi posuere neque diam. Nulla laoreet tortor ut varius mollis. Donec mattis enim lacus, a euismod est efficitur vitae. Pellentesque egestas sed mi nec dapibus.
+			</div>
+			<h3><a href=\"#\">Seção 4</a></h3>
+			<div>
+				In dui mauris, interdum vel bibendum eu, faucibus et turpis. Praesent elit enim, sollicitudin at maximus eu, cursus non nulla. Vivamus nisi lorem, pellentesque id fringilla non, dignissim eu orci. In mollis sit amet velit vel fringilla. Etiam pellentesque arcu nibh, non placerat ante hendrerit a. Nunc accumsan quam turpis, nec facilisis lorem porttitor at. Vestibulum dictum mi a arcu finibus, id dignissim quam placerat. Nulla nec libero quam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec euismod venenatis elit sed vestibulum. Proin varius purus justo, semper tincidunt nisi aliquam id. Maecenas et lacus urna.
+			</div>
+			
+		</div>"
+		?>
+	</body>
+	
+</html>
